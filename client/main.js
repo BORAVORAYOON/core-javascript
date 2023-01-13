c070dd370c42784822e0462e095f1091522b6373
BORAVORAYOON <yoonbora1803@naver.com>
2023-01-13 15:53:50 +0900

    주접생성기 완성

--- client/main.js
+++ client/main.js
@@ -1,1 +1,41 @@
-{ }
+
+import { getInputValue, getNode, getRandom, insertLast, clearContents, isNumericString, showAlert } from './lib/index.js'
+
+import { jujeobData } from './data/data.js';
+
+
+
+
+const submit = getNode('#submit');
+const resultArea = getNode('.result')
+
+console.log(submit);
+
+console.log(getRandom(5))
+
+
+function clickSubmitHandler(e) {
+  e.preventDefault();
+
+  let name = getInputValue('#nameField');
+  let list = jujeobData(name)
+  let pick = list[getRandom(list.length - 1)]
+
+  if (!name) {
+    showAlert('.alert', '이름을 입력해주세요.', 3000)
+    return
+  }
+
+  if (isNumericString(name)) {
+    showAlert('.alert', '제대로된 이름을 입력해주세요.', 3000)
+    return
+  }
+
+
+  console.log(pick);
+
+  clearContents('.result');
+  insertLast('.result', pick);
+}
+
+submit.addEventListener('click', clickSubmitHandler);
