e1172013dfd206b795e63acac0e3eacc029d2370
BORAVORAYOON <yoonbora1803@naver.com>
2023-01-17 09:14:24 +0900

    랜덤다이스 완성

---
+++ client/lib/utils/memo.js
@@ -0,0 +1,20 @@
+
+
+const memo = (() => {
+  const cache = {}
+
+  return (key, callback) => {
+    if (!callback) return cache[key];
+    if (cache[key]) {
+      console.warn(`${key}이미 캐시된 값이 존재합니다.`);
+      return;
+    }
+
+    cache[key] = callback();
+
+    // console.log(cache);
+  }
+})()
+
+
+memo('cube', () => document.querySelector('#cube'));
